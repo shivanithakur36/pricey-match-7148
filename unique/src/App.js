0eba36c95f0415a46a6834d6461b9258dcb0ecae
shivanithakur36 <shivanivijaythakur@gmail.com>
2022-11-12 00:56:37 +0530

    signup page also added

--- unique/src/App.js
+++ unique/src/App.js
@@ -1,16 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from "./Components/Navbar/Navbar";
+// import Navbar from "./Components/Navbar/Navbar";
 // import GetStartedPage from "./Components/MiddlePage/MiddlePage";
 import Login from "./Components/Pages/Login";
-import Footer from "./Components/Footer/Footer"
+import Footer from "./Components/Footer/Footer";
+import Signup from "./Components/Pages/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
       <Login/>
       <Footer/>
+      <Signup/>
       {/* <GetStartedPage/> */}
       {/* <MiddlePage/> */}
       
